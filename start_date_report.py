2aa2041c5c75aee5dda6de96869c1317db3cac64
manabil <mammarnabil@students.unnes.ac.id>
2023-03-16 15:46:07 +0700

    jospol

--- start_date_report.py
+++ start_date_report.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 import csv
 import datetime
 import requests
@@ -8,7 +7,7 @@ import requests
 
 FILE_URL = "https://storage.googleapis.com/gwg-content/gic215/employees-with-date.csv"
 
-def get_start_date():
+def get_start_date() -> datetime.datetime:
     """Interactively get the start date to query for."""
 
     print()
@@ -22,7 +21,7 @@ def get_start_date():
 
     return datetime.datetime(year, month, day)
 
-def get_file_lines(url):
+def get_file_lines(url: str) -> list:
     """Returns the lines contained in the file at the given URL"""
 
     # Download the file over the internet
@@ -33,52 +32,36 @@ def get_file_lines(url):
         lines.append(line.decode("UTF-8"))
     return lines
 
-def get_same_or_newer(start_date, reader):
-    """Returns the employees that started on the given date, or the closest one."""
-    # We want all employees that started at the same date or the closest newer
-    # date. To calculate that, we go through all the data and find the
-    # employees that started on the smallest date that's equal or bigger than
-    # the given start date.
-    min_date = datetime.datetime.today()
-    min_date_employees = []
-    for row in reader: 
-        row_date = datetime.datetime.strptime(row[3], '%Y-%m-%d')
-
-        # If this date is smaller than the one we're looking for,
-        # we skip this row
-        if row_date < start_date:
-            continue
-
-        # If this date is smaller than the current minimum,
-        # we pick it as the new minimum, resetting the list of
-        # employees at the minimal date.
-        if row_date < min_date:
-            min_date = row_date
-            min_date_employees = []
-
-        # If this date is the same as the current minimum,
-        # we add the employee in this row to the list of
-        # employees at the minimal date.
-        #print(min_date_employees)
-        if row_date == min_date:
-            min_date_employees.append("{} {}".format(row[0], row[1]))
-
-    return min_date, min_date_employees
-
-def list_newer(start_date):
+def get_ordered_data() -> dict:
     data = get_file_lines(FILE_URL)
     reader = csv.reader(data[1:])
-    while start_date < datetime.datetime.today():
-        start_date, employees = get_same_or_newer(start_date, reader)
-        print("Started on {}: {}".format(start_date.strftime("%b %d, %Y"), employees))
+    data_dict = {}
+    
+    for row in reader:
+        if row[3] not in data_dict:
+            data_dict.update({row[3]: [f"{row[0]} {row[1]}"]})
+        else:
+            data_dict[row[3]].append(f"{row[0]} {row[1]}")
+           
+    data_dict = sort_dict(data_dict)
+    return data_dict
 
-        # Now move the date to the next one
-        start_date = start_date + datetime.timedelta(days=1)
+def sort_dict(data: dict) -> dict:
+    myKeys = list(data.keys())
+    myKeys.sort()
+    sorted_dict = {i: data[i] for i in myKeys}
+    return sorted_dict
+
+def get_employee(datas: dict, start_date: datetime.datetime) -> None:
+    str_date = start_date.strftime("%Y-%m-%d")
+    for data in datas.items():
+        if data[0] >= str_date:
+            print(f"Started on {data[0]}: {data[1]}")
 
 def main():
     start_date = get_start_date()
-    get_same_or_newer(start_date)
-    #list_newer(start_date)
+    data = get_ordered_data()
+    get_employee(data, start_date)
 
 if __name__ == "__main__":
     main()
